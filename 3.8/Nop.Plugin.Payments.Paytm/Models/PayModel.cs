178483d58f4236030456a0621d1ab3bfe0a20305
MayankGupta1 <mayank1.gupta@paytm.com>
2017-01-27 15:07:24 +0530

    nopcommerce v 3.8

---
+++ 3.8/Nop.Plugin.Payments.Paytm/Models/PayModel.cs
@@ -0,0 +1,11 @@
+﻿using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.Payments.Paytm.Models
+{
+    public class PayModel : BaseNopModel
+    {
+        public string RespCode { get; internal set; }
+        public string RespMsg { get; internal set; }
+        public string OrderId { get; internal set; }
+    }
+}
