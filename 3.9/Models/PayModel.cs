0f789e8ccb152e52cf9fbd6a4c427c0360acf451
MayankGupta1 <mayank1.gupta@paytm.com>
2017-08-28 16:16:48 +0530

    Updated

---
+++ 3.9/Models/PayModel.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.Payments.Paytm.Models
+{
+    public class PayModel : BaseNopModel
+    {
+        public string RespMsg { get; set; }
+    }
+}
