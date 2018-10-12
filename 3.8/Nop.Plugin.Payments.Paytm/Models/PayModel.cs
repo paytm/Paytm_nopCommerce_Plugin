715ad695356921680a7a9f460b1462bf3d6f118b
Lalit Kumar <lalit3.kumar@paytm.com>
2018-10-12 11:17:42 +0530

    remove deprecated version 3.8

--- 3.8/Nop.Plugin.Payments.Paytm/Models/PayModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Web.Framework.Mvc;
-
-namespace Nop.Plugin.Payments.Paytm.Models
-{
-    public class PayModel : BaseNopModel
-    {
-        public string RespMsg { get; set; }
-    }
-}
