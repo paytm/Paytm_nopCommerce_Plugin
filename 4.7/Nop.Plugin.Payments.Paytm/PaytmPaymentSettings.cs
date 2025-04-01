27f542c2a34ac718783324e0a4b947a1c58fe2ce
Chandra Shekhar Garsari <chandra.garsari@paytmpayments.com>
2025-04-01 15:46:12 +0530

    4.7 supported version

---
+++ 4.7/Nop.Plugin.Payments.Paytm/PaytmPaymentSettings.cs
@@ -0,0 +1,26 @@
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Payments.Paytm
+{
+    public class PaytmPaymentSettings : ISettings
+    {
+        public string MerchantId { get; set; }
+        public string MerchantKey { get; set; }
+        public string Website { get; set; }
+        public string IndustryTypeId { get; set; }
+        public string PaymentUrl { get; set; }
+        public string CallBackUrl { get; set; }
+        public string TxnStatusUrl { get; set; }
+        public bool UseDefaultCallBack { get; set; }
+        public string PdtToken { get; set; }
+        public decimal AdditionalFee { get; set; }
+
+        public bool AdditionalFeePercentage { get; set; }
+        public string env { get; set; }
+        public string webhook { get; set; }
+
+        public string EmiSubvention { get; set; }
+        public string BankOffers { get; set; }
+        public string DcEmi { get; set; }
+    }
+}
---
+++ 4.7/Nop.Plugin.Payments.Paytm/Services/PaytmHttpClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
+using Nop.Core;
+
+namespace Nop.Plugin.Payments.Paytm.Services
+{
+    public partial class PaytmHttpClient
+    {
+        #region Fields
+
+        private readonly HttpClient _httpClient;
+        private readonly PaytmPaymentSettings _paytmPaymentSettings;
+
+        #endregion
+
+        #region Ctor
+
+        public PaytmHttpClient(HttpClient client,
+            PaytmPaymentSettings paytmPaymentSettings)
+        {
+            //configure client
+            client.Timeout = TimeSpan.FromSeconds(20);
+            client.DefaultRequestHeaders.Add(HeaderNames.UserAgent, $"nopCommerce-{NopVersion.CURRENT_VERSION}");
+
+            _httpClient = client;
+            _paytmPaymentSettings = paytmPaymentSettings;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets PDT details
+        /// </summary>
+        /// <param name="tx">TX</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the asynchronous task whose result contains the PDT details
+        /// </returns>
+        public async Task<string> GetPdtDetailsAsync(string tx)
+        {
+            //get response
+            var url = "https://www.paytm.com";
+            var requestContent = new StringContent($"cmd=_notify-synch&at={_paytmPaymentSettings.PdtToken}&tx={tx}",
+                Encoding.UTF8, MimeTypes.ApplicationXWwwFormUrlencoded);
+            var response = await _httpClient.PostAsync(url, requestContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// Verifies IPN
+        /// </summary>
+        /// <param name="formString">Form string</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the asynchronous task whose result contains the IPN verification details
+        /// </returns>
+        public async Task<string> VerifyIpnAsync(string formString)
+        {
+            //get response
+            var url = "https://www.paytm.com";
+            var requestContent = new StringContent($"cmd=_notify-validate&{formString}",
+                Encoding.UTF8, MimeTypes.ApplicationXWwwFormUrlencoded);
+            var response = await _httpClient.PostAsync(url, requestContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        #endregion
+    }
+}
